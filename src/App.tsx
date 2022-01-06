2da2a906a4df3ca17bbf4951dcb1a2f41b891d96
ericledonge <eric.ledonge@gmail.com>
2022-01-05 20:15:10 -0500

    Project completed
    Project completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
 import './App.css';
 
-import React from 'react';
+import React, { useState } from 'react';
 
-import Hello from './components/Hello';
+const App: React.FC = () => {
+  const [price, setPrice] = useState(50);
 
-function App() {
-  return <Hello name="Éric" />;
-}
+  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setPrice(parseFloat(event.currentTarget.value));
+  };
+
+  const locale = document.documentElement.lang;
+
+  const convertToCurrency = (price: number) =>
+    new Intl.NumberFormat(locale, { style: 'currency', currency: 'USD' }).format(price);
+
+  return (
+    <div className="wrapper">
+      <div className="amount">
+        <span className="dollars">{convertToCurrency(price)}</span>
+      </div>
+      <input
+        type="range"
+        id="priceRange"
+        min="0"
+        max="100"
+        step="0.01"
+        value={price}
+        onChange={(e) => handleChange(e)}
+      />
+      <br />
+      <button>Buy Now</button>
+    </div>
+  );
+};
 
 export default App;
